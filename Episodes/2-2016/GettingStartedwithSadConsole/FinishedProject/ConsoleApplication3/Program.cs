751515f1dfb9df4d99d0ec5e60ac7c0707a2de47
Stacey Haffner <sthaff@microsoft.com>
2017-02-10 08:11:01 -0800

    Adding SadConsole Episode Artifcats

---
+++ Episodes/2-2016/GettingStartedwithSadConsole/FinishedProject/ConsoleApplication3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Console = SadConsole.Consoles.Console;
+using SadConsole.Consoles;
+using Microsoft.Xna.Framework;
+
+namespace MyNamespace
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Setup the engine and creat the main window.
+            SadConsole.Engine.Initialize("IBM.font", 80, 25);
+
+            // Hook the start event so we can add consoles to the system.
+            SadConsole.Engine.EngineStart += Engine_EngineStart;
+
+            // Hook the update event that happens each frame so we can trap keys and respond.
+            SadConsole.Engine.EngineUpdated += Engine_EngineUpdated;
+
+            // Start the game.
+            SadConsole.Engine.Run();
+        }
+
+        private static void Engine_EngineStart(object sender, EventArgs e)
+        {
+            var defaultConsole = (Console)SadConsole.Engine.ActiveConsole;
+            defaultConsole.TextSurface = new TextSurface(80, 12);
+            defaultConsole.Fill(Color.Blue, Color.Yellow, 0);
+            defaultConsole.Print(1, 1, "Hello from console 1");
+
+            var console2 = new Console(80, 13);
+            console2.Position = new Point(0, 12);
+            console2.Fill(Color.Yellow, Color.Blue, 0);
+            console2.Print(1, 1, "Hello from console 2");
+
+            SadConsole.Engine.ConsoleRenderStack.Add(console2);
+
+            SadConsole.Engine.ToggleFullScreen();
+
+
+        }
+
+        private static void Engine_EngineUpdated(object sender, EventArgs e)
+        {
+            if (SadConsole.Engine.Keyboard.IsKeyReleased(Microsoft.Xna.Framework.Input.Keys.Escape))
+                SadConsole.Engine.MonoGameInstance.Exit();
+        }
+    }
+}
---
+++ Episodes/2-2016/GettingStartedwithSadConsole/readme.md
@@ -0,0 +1,12 @@
+
+# Getting Started with SadConsole
+
+In this [episode](https://channel9.msdn.com/Shows/dotGAME/Getting-Started-with-SadConsole), wAndy De George joins us to talk about SadConsole. SadConsole is a MonoGame 3.5-based game library (using .NET 4.5) that provides an engine to emulate old-school console and command prompt style graphics. To start, we will look at how you get up and running within minutes. Then we'll explore some of the basic functionality and principles such as working with multiple consoles, altering the text color and interact with inputs. 
+
+[![screenshot](screenshot.png)](https://channel9.msdn.com/Shows/dotGAME/Getting-Started-with-SadConsole)
+
+# Resources
+
+* [SadConsole GitHub Repo](https://github.com/Thraka/SadConsole)
+* [SadConsole Editor GitHub Repo](https://github.com/Thraka/SadConsoleEditor/)
+* [SadConsole SubReddit](https://channel9.msdn.com/Shows/dotGAME/reddit.com/r/sadconsole)
